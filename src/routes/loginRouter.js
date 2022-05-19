35c452cdfe8b0d47d678ba62ff66a194de916dba
pedro imbrech <elprogramadorpedro@gmail.com>
2022-05-19 16:32:19 -0300

    rev

--- src/routes/loginRouter.js
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express'
-
-import postLogin from './../controllers/loginController.js'
-
-const loginRouter = Router()
-
-loginRouter.post("/login", postLogin)
-
-export default loginRouter
